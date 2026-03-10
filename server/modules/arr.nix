2b1e8a91f529415e8f54826bf4e98ee0516eb6c7
kin <kinestt@proton.me>
2026-03-10 15:33:50 +0530

    commit

--- server/modules/arr.nix
+++ server/modules/arr.nix
@@ -1,13 +1,16 @@
 {
   services = {
     radarr = {
-      enable = true;  
+      enable = true;
+      server.port = 7878; 
     };
     sonarr = {
       enable = true;
+      server.port = 8989;
     };
     qbittorrent = {
       enable = true;
+      webuiPort = 8888;
       extraArgs = 
         [
           "--confirm-legal-notice"
