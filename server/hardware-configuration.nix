551cce7b1135ee7f34b9679f8b877980cde9009d
kin <kinestt@proton.me>
2026-03-21 17:00:15 +0530

    commit

--- server/hardware-configuration.nix
+++ server/hardware-configuration.nix
@@ -26,6 +26,7 @@
   fileSystems."/mnt/hdd" = {
     device = "/dev/disk/by-uuid/E2AEBCF3AEBCC0F9";
     fsType = "ntfs3";
+    options = [ "defaults" "nofail" ];
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
