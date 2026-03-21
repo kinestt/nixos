9173ed3651d92eb24cbb4c9eecd0eaa88a3d0ef4
kin <kinestt@proton.me>
2026-03-21 12:08:57 +0530

    commit

--- laptop/home-manager/modules/bash.nix
+++ laptop/home-manager/modules/bash.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   programs.bash = {
     enable = true;
     shellAliases = {
       hms = "home-manager switch --flake /home/kin/nixos/#kin-laptop";
       rebuild = "sudo nixos-rebuild switch --flake /home/kin/nixos#laptop";
-      pushgit = ''git -C /home/kin/nixos add . && git -C /home/kin/nixos commit -m "commit" && git -C /home/kin/nixos push'';
+      pushgit = ''git -C ${config.home.homeDirectory}/nixos add . && git -C ${config.home.homeDirectory}/nixos commit -m "commit" && git -C ${config.home.homeDirectory}/nixos push'';
     };
     bashrcExtra = ''
       export SSH_AUTH_SOCK=/home/kin/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
