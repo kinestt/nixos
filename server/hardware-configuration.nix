03a80d2311d5bcacb75312eb1ceb76fd0e1716e4
kin <kinestt@proton.me>
2026-03-21 17:46:50 +0530

    commit

--- server/hardware-configuration.nix
+++ server/hardware-configuration.nix
@@ -25,7 +25,7 @@
     };
   fileSystems."/home/kin/mount" = {
     device = "/dev/disk/by-uuid/E2AEBCF3AEBCC0F9";
-    fsType = "ntfs3";
+    fsType = "ntfs-3g";
     options = [ "nofail" "rw" "umask=000" "uid=1000" "gid=100" ];
   };
 
