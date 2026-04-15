28e6ec4801e526fcdb726475e482db609bd53e1b
kinestt <kinestt@proton.me>
2026-04-15 11:01:15 +0530

    commit

--- server/home-manager/modules/bash.nix
+++ server/home-manager/modules/bash.nix
@@ -4,7 +4,8 @@
     shellAliases = {
       hms = "home-manager switch --flake /home/kin/nixos/#kin-server";
       rebuild = "sudo nixos-rebuild switch --flake /home/kin/nixos#server";
-      pushgit = ''git -C ${config.home.homeDirectory}/nixos add . && git -C ${config.home.homeDirectory}/nixos commit -m "commit" && git -C ${config.home.homeDirectory}/nixos push'';
+      pushmain = ''git -C ${config.home.homeDirectory}/nixos add . && git -C ${config.home.homeDirectory}/nixos commit -m "commit" && git -C ${config.home.homeDirectory}/nixos push'';
+      pushsecret = ''git -C ${config.home.homeDirectory}/nix-secrets add . && git -C ${config.home.homeDirectory}/nix-secrets commit -m "commit" && git -C ${config.home.homeDirectory}/nix-secrets push'';
     };
   };
 }
