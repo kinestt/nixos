fdb40a3537c2eaa11e7a2a36989f3a1c1e85db60
kin <kinest@tutamail.com>
2026-06-05 10:00:57 +0530

    commit

--- modules/home/home-modules/waybar.nix
+++ modules/home/home-modules/waybar.nix
@@ -34,7 +34,7 @@
       style = "
         #waybar {
           font-family: 'IBM Plex Sans', 'Iosevka Nerd Font Propo' ;
-          font-size: 14px;
+          font-size: 16px;
           font-weight: normal;
           background-color: #191724;
         }
