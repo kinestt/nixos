b73908ab4754b0aa345adf08c0666efbfe98c33d
kinestt <kinestt@proton.me>
2026-06-08 12:34:30 +0530

    commit

--- modules/hosts/server/configuration.nix
+++ modules/hosts/server/configuration.nix
@@ -27,7 +27,7 @@
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    #boot.kernelPackages = pkgs.linuxPackages_latest;
     nix.settings.experimental-features = ["nix-command" "flakes"];
     users.users.kin = {
       isNormalUser = true;
