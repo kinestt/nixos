a10289bfc402b5eb1de4fbe5b4b773832c8ac7e9
kin <kinest@tutamail.com>
2026-06-28 12:30:01 +0530

    changed linkwarden host

--- modules/features/web/linkwarden.nix
+++ modules/features/web/linkwarden.nix
@@ -13,6 +13,7 @@
     };
     services.linkwarden = {
       enable = true;
+      host = "0.0.0.0";
       secretFiles = {
         NEXTAUTH_SECRET = config.sops.secrets."linkwarden/nextauth_secret".path;
         POSTGRES_PASSWORD = config.sops.secrets."linkwarden/postgres_password".path;
