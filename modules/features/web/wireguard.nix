2c21ded70ed6189662c7f7d818964767ea95d276
kin <kinest@tutamail.com>
2026-07-01 16:45:01 +0530

    commit

--- modules/features/web/wireguard.nix
+++ modules/features/web/wireguard.nix
@@ -37,13 +37,13 @@
           ];
           postSetup = ''
             ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eno1 -j MASQUERADE
           '';
 
       # Undo the above
           postShutdown = ''
             ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eno1 -j MASQUERADE
           '';
         };
       };
