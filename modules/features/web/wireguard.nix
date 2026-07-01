d73e4a14c2ac0c9dfa500c6c1a739d8089d1aceb
kin <kinest@tutamail.com>
2026-07-01 16:49:59 +0530

    commit

--- modules/features/web/wireguard.nix
+++ modules/features/web/wireguard.nix
@@ -28,11 +28,8 @@
           privateKeyFile = config.sops.secrets."wireguard/privatekey".path;
           peers = [
             {
-              name = "op13";
               publicKey = "YJSmpyuD9hdgh4sieucEJI73r0g+BtdofetE0GOrnQk=";
               allowedIPs = [ "10.0.0.2/24" ];
-              endpoint = "192.168.0.122:51820";
-              persistentKeepalive = 25;
             }
           ];
           postSetup = ''
