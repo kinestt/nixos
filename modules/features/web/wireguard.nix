0f8fd8e59d1ef03dd20a4fa39b9d2e035f4af23f
kin <kinest@tutamail.com>
2026-07-01 17:17:52 +0530

    commit

--- modules/features/web/wireguard.nix
+++ modules/features/web/wireguard.nix
@@ -27,27 +27,19 @@
 
     networking.wireguard = {
       enable = true;
+
       interfaces = {
         wg0 = {
           ips = [ "10.0.0.1/24" ];
           listenPort = 51820;
           privateKeyFile = config.sops.secrets."wireguard/privatekey".path;
+
           peers = [
             {
               publicKey = "YJSmpyuD9hdgh4sieucEJI73r0g+BtdofetE0GOrnQk=";
               allowedIPs = [ "10.0.0.2/32" ];
             }
           ];
-          postSetup = ''
-            ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eno1 -j MASQUERADE
-          '';
-
-      # Undo the above
-          postShutdown = ''
-            ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o eno1 -j MASQUERADE
-          '';
         };
       };
     };
