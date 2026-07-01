55b5a0b4f96188fe3af45dc4c48b71a1b22b3d6b
kin <kinest@tutamail.com>
2026-07-01 16:42:54 +0530

    commit

--- modules/features/web/wireguard.nix
+++ modules/features/web/wireguard.nix
@@ -23,14 +23,14 @@
       enable = true;
       interfaces = {
         wg0 = {
-          ips = [ "10.100.0.2/24" ];
+          ips = [ "10.0.0.1/24" ];
           listenPort = 51820;
           privateKeyFile = config.sops.secrets."wireguard/privatekey".path;
           peers = [
             {
               name = "op13";
               publicKey = "YJSmpyuD9hdgh4sieucEJI73r0g+BtdofetE0GOrnQk=";
-              allowedIPs = [ "10.100.0.0/24" ];
+              allowedIPs = [ "10.0.0.2/24" ];
               endpoint = "192.168.0.122:51820";
               persistentKeepalive = 25;
             }
