c37efcf1ffa1f1939981c2e0f5c299df11c73a93
kin <kinest@tutamail.com>
2026-07-09 22:43:33 +0530

    commit

--- modules/features/web/copyparty.nix
+++ modules/features/web/copyparty.nix
@@ -4,11 +4,29 @@
   ...
 }: {
   flake.nixosModules.copyparty = {
+    config,
+    ...
+  }: {
     imports = [
       inputs.copyparty.nixosModules.default
     ];
+    sops.secrets = {
+      "copyparty/kin/password" = {};
+    };
     services.copyparty = {
       enable = true;
+      user = "kin";
+      group = "users";
+      accounts = {
+        kin.passwordFile = config.sops.secrets."copyparty/kin/password".path;
+      };
+      volumes = {
+        "/" = {
+          access = {
+            rw = [ "kin" ];
+          };
+        };
+      };
     };
   };
 }
