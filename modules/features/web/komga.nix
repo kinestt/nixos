1c801a9256a04c096e11707dac98c3f0ea53f3da
kin <kinest@tutamail.com>
2026-07-19 14:38:52 +0530

    commit

--- modules/features/web/komga.nix
+++ modules/features/web/komga.nix
@@ -6,6 +6,8 @@
   flake.nixosModules.komga = {
     services.komga = {
       enable = true;
+      user = "media";
+      group = "media";
       settings = {
         server = {
           port = 10060;
